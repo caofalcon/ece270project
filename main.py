9b4a8c913abeca828df40cea794901817bdac2a5
Rahul Chandan <luhar.nadnahc@gmail.com>
2018-05-30 22:07:58 -0700

    working with agent classes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import numpy as np
 from joblib import Parallel, delayed
 import multiprocessing
 
-MAX_RESOURCE = 10
+TIME_HORIZON = 100
+NUM_PLAYERS = 12
+MAX_RESOURCE = 40
 NUM_RESOURCES = 4
 
 alpha = 0.1
@@ -11,16 +13,13 @@ max_occ = np.array([5,5,8,2])
 player_structs = np.ones((4,4)) + np.eye(4)*3
 
 def policyImprovement(i, occ, J):
-    mu = np.arange(4, dtype=int)
+    # mu = np.arange(4, dtype=int)
     order = MAX_RESOURCE ** np.arange(NUM_RESOURCES)
     # incr is 1 if max_occ is larger than occ (i.e. max_occ hasn't been met)
     # and is 0 otherwise
-    print('policyEvaluation:', mu.size)
-    print('policyEvaluation:', occ[:,mu].size)
-    incr = np.argmin([max_occ[mu], occ[mu]], axis=0) * player_structs[i % NUM_RESOURCES]
-    print('project.py:policyImprovement(): player ', i)
-    print('project.py:policyImprovement(): increment ', incr)
-    print('project.py:policyImprovement(): occupancy ', occ)
+    incr = np.argmin([np.tile(max_occ, (MAX_RESOURCE ** NUM_RESOURCES, 1)), occ], axis=0) * player_structs[i % NUM_RESOURCES]
+    # print('project.py:policyImprovement(): increment ', incr.shape)
+    # print('project.py:policyImprovement(): occupancy ', occ.shape)
 
     x = np.arange(np.size(J))
     x = np.transpose(np.tile(x, (NUM_RESOURCES,1)))
@@ -30,13 +29,12 @@ def policyImprovement(i, occ, J):
                               (x/order%MAX_RESOURCE-incr).astype(int)*order], axis=0)
     x_next = x_next.astype(int)
 
-    print('project.py:policyImprovement():', x_next[9999,:])
+    # print('project.py:policyImprovement():', x_next[9999,:])
 
     H = np.transpose(np.sum((np.transpose(np.tile(x_next, (NUM_RESOURCES,1,1)), (2,1,0))/order%MAX_RESOURCE).astype(int), axis=2))
-    # print(H)
-    print('project.py:policyImprovement():', H[9999,:])
+    # print('project.py:policyImprovement():', H[9999,:])
     H = np.transpose(H ** 2 + alpha*J[x_next])
-    print('project.py:policyImprovement():', H[:,9999])
+    # print('project.py:policyImprovement():', H[:,9999])
     # print('project.py:policyImprovement:', H[:,9999])
     return (np.min(H, axis=0), np.argmin(H, axis=0))
 
@@ -44,9 +42,11 @@ def policyEvaluation(i, occ, mu, J, V):
     # H(x,mu,J) = g(x,mu) + a*J(f(x,mu))
     # J[t+1, i] = min(V, H(x,mu,J[t, :]))
     order = MAX_RESOURCE ** mu
-    print('policyEvaluation:', mu.size)
-    print('policyEvaluation:', occ[mu.astype(int)].size)
-    incr = np.argmin([max_occ[mu.astype(int)], occ[mu.astype(int)]], axis=0) * player_structs[i % NUM_RESOURCES, mu.astype(int)]
+    # print('project.py:policyEvaluation():', mu.shape)
+    # print('project.py:policyEvaluation():', mu[0])
+    # print('project.py:policyEvaluation():', occ[np.arange(MAX_RESOURCE**NUM_RESOURCES), mu.astype(int)].shape)
+    # print('project.py:policyEvaluation():', occ[np.arange(MAX_RESOURCE**NUM_RESOURCES), mu.astype(int)][0])
+    incr = np.argmin([max_occ[mu.astype(int)], occ[np.arange(MAX_RESOURCE**NUM_RESOURCES), mu.astype(int)]], axis=0) * player_structs[i % NUM_RESOURCES, mu.astype(int)]
     # print (i,occ, incr)
     x = np.arange(np.size(J))
     x_next = x - (x / order % MAX_RESOURCE).astype(int) * order
@@ -60,24 +60,20 @@ def policyEvaluation(i, occ, mu, J, V):
     return np.min([V, H], axis=0)
 
 def main():
-    time_horizon = 100
-    num_players = 12
-    num_resources = NUM_RESOURCES
-
-
-    T = np.random.randint(2, size=(time_horizon, num_players))
-    T_bar = np.random.randint(2, size=(time_horizon, num_players))
+    T = np.random.randint(2, size=(TIME_HORIZON, NUM_PLAYERS))
+    T_bar = np.random.randint(2, size=(TIME_HORIZON, NUM_PLAYERS))
     
     # Policy will be per player, dependent on x = R_j - S_j, and is
     # initialized to be uniformly-distributed across all resources
-    policy = np.zeros((time_horizon, num_players, MAX_RESOURCE ** num_resources))
-    J = np.zeros((time_horizon, num_players, MAX_RESOURCE ** num_resources))
-    V = (time_horizon*NUM_RESOURCES*MAX_RESOURCE)**2*np.ones((num_players, MAX_RESOURCE ** num_resources))
+    policy = np.random.randint(NUM_RESOURCES, size=(TIME_HORIZON, NUM_PLAYERS, MAX_RESOURCE ** NUM_RESOURCES))
+    J = np.zeros((TIME_HORIZON, NUM_PLAYERS, MAX_RESOURCE ** NUM_RESOURCES))
+    V = (TIME_HORIZON*NUM_RESOURCES*MAX_RESOURCE)**2*np.ones((NUM_PLAYERS, MAX_RESOURCE ** NUM_RESOURCES))
     
-    occ = np.zeros((MAX_RESOURCE ** num_resources, NUM_RESOURCES))
+    occ = np.zeros((MAX_RESOURCE ** NUM_RESOURCES, NUM_RESOURCES))
 
-    for t in range(time_horizon-1):
-        for i in range(num_players):
+    for t in range(TIME_HORIZON-1):
+        for i in range(NUM_PLAYERS):
+            # print('project.py:main(): player ', i)
             if T_bar[t,i] > 0:
                 (J[t+1,i], policy[t+1,i]) = policyImprovement(i, occ, J[t,i])
                 V[i] = J[t+1,i]
@@ -88,12 +84,13 @@ def main():
                 J[t+1,i] = J[t,i]
                 policy[t+1,i] = policy[t,i]
             # occ[policy.astype(int)[t+1,i]] = occ[policy.astype(int)[t+1,i]] + 1
-            temp = np.tile(np.arange(NUM_RESOURCES), (MAX_RESOURCE ** NUM_RESOURCES, 1))
-            occ = occ + np.transpose(np.transpose(temp) == policy)
-            print('project.py:main(): policy ', policy.astype(int)[t+1,i])
-            print('project.py:main(): occ ', occ)
-        occ = np.zeros(NUM_RESOURCES)
-        break
+            temp = 2 ** np.arange(NUM_RESOURCES)
+            temp = (np.transpose(np.tile(2**policy[t+1,i], (NUM_RESOURCES,1))) / temp % 2).astype(int)
+            occ = occ + temp
+            # print('project.py:main(): policy ', policy[t+1,i,0])
+            # print('project.py:main(): temp ', temp.shape)
+            # print('project.py:main(): occ ', occ)
+        occ = np.zeros((MAX_RESOURCE ** NUM_RESOURCES, NUM_RESOURCES))
     np.save('J.npy', J)
     np.save('mu.npy', policy)
 
